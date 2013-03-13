1f3cf3cc8b28d79e5eef2430f68d54e32385de27
Ismo Toijala <ismo.toijala@gmail.com>
2013-03-13 10:44:25 +0100

    test for point label

--- test2.py
+++ test2.py
@@ -15,5 +15,10 @@ l5 = d.add(Line(p2, (4, -2)))
 l6 = d.add(Line((3, 1), (3, -1), bend=-0.5, deformer="coil"))
 l7 = d.add(Line((-3, 1), (-3, -1), bend=0.5, deformer="doublesineline"))
 lab = d.add(Label(r"${x}{y^2_3}$", 30, 0, 1))
-p1.add_label("test")
+#p1.add_label("test")
+l3.add_label("foo")
+
+pl = d.add(Point(0, -1.3, marker="circle"))
+pl.add_label(Label("texg", angle=45), angle=170)
+
 d.save("a.pdf")
